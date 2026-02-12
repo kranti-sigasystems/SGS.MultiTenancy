e4af373de17f8fefabe22a0843e5b26722627287
sominathg-jpg <sominathg@sigasystems.com>
2026-02-12 12:03:18 +0530

    Configurations for serilog

---
+++ SGS.MultiTenancy.UI/Logging/LoggingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+
+namespace SGS.MultiTenancy.UI.Infrastructure.Logging;
+
+public static class LoggingExtensions
+{
+    public static WebApplicationBuilder AddSerilogLogging(this WebApplicationBuilder builder)
+    {
+        builder.Host.UseSerilog((context, services, config) =>
+           {
+               config
+                   .ReadFrom.Configuration(context.Configuration)
+                   .ReadFrom.Services(services)
+                   .Enrich.FromLogContext()
+                   .Enrich.WithMachineName()
+                   .Enrich.WithThreadId();
+           });
+
+        return builder;
+    }
+}
