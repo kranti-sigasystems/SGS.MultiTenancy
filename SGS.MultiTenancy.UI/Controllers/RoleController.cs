011d920c776824457d06b1de21fafa6d431ec080
sominathg-jpg <sominathg@sigasystems.com>
2026-02-23 12:14:11 +0530

    User and tenant UI consistent.

---
+++ SGS.MultiTenancy.UI/Controllers/RoleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SGS.MultiTenancy.UI.Controllers
+{
+    public class RoleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
