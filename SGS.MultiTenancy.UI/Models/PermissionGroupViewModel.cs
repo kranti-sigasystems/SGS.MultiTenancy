04e5a47b241c86a3e408def74712ade59b597009
Pratik N <pratikn@sigasystems.com>
2026-03-03 15:55:53 +0530

    Group view model comments

--- SGS.MultiTenancy.UI/Models/PermissionGroupViewModel.cs
+++ SGS.MultiTenancy.UI/Models/PermissionGroupViewModel.cs
@@ -2,7 +2,14 @@
 {
     public class PermissionGroupViewModel
     {
+        /// <summary>
+        /// Gets or sets the name of the permission group.
+        /// </summary>
         public string GroupName { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets the list of permissions that belong to this group.
+        /// </summary>
         public List<PermissionItemViewModel> Permissions { get; set; } = new();
     }
 }
